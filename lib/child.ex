553b2b7db08d991c2f17066625b615378d0c9f69
Michael Kirschbaum <mkirsch@clemson.edu>
2015-06-03 23:42:41 -0700

    kill processes

--- lib/child.ex
+++ lib/child.ex
@@ -1,25 +1,32 @@
 defmodule Child do
-   def start do
+   def start(parent) do
+      is_sociopath = :random.uniform < 0.04
+      
       receive do
-         {:neighbor, pid} -> hotpotato(pid)
+         {:neighbor, pid} -> hotpotato({pid, parent, is_sociopath})
       end
    end
    
-   def hotpotato(neighbor) do
+   def hotpotato(state) do
+      {neighbor, parent, is_sociopath} = state 
       receive do
-         {:getpotato} -> havepotato(neighbor)
-	                 hotpotato(neighbor);
-	 {:timeout, _} -> hotpotato(neighbor);
-	 {:neighbor, pid} -> hotpotato(pid);
+         {:getpotato} -> havepotato(state)
+	                 hotpotato(state);
+	 {:timeout, _} -> hotpotato(state);
+	 {:neighbor, pid} -> hotpotato({pid, parent, is_sociopath});
 	 {:kill} -> ()
       end
    end
 
-   def havepotato(neighbor) do
+   def havepotato({neighbor, parent, is_sociopath}) do
       receive do
          {:timeout, parent} -> send(parent, {:loser, self})
          after
-	    87 -> send(neighbor, {:getpotato})
+	    100 -> if is_sociopath do
+	              send(parent, {:grenade})
+		   else
+	              send(neighbor, {:getpotato})
+		   end
       end
    end
 end
