51443697576035d09d9e346cf6671500b48c4c21
Alec Hollingsworth <alec@mailc.net>
2015-06-03 20:30:22 -0700

    fix the babysitter

--- lib/babysitter.ex
+++ lib/babysitter.ex
@@ -20,20 +20,34 @@ defmodule Babysitter do
     potato = worker(Potato, [], id: make_ref())
 
     children = [potato] ++ children
+
+    IO.puts "Sitter is about to start the game"
+
+    # supervise(children, strategy: :one_for_all, max_restarts: 3)
     supervise(children, strategy: :one_for_one)
-    # IO.inspect children
+
+    IO.inspect children
+    IO.puts "Sitter started supervising"
+
+    # play(self)
   end
 
   def play(pid) do
+    IO.puts "Sitter has started the game!"
+    IO.inspect pid
+
     all_processes = Supervisor.which_children(pid)
+
+    IO.inspect all_processes
 
     [child_processes, [potato_process]] = Enum.chunk_by(all_processes, fn({_, _, _, [type]}) -> type == Child end)
 
     child_pids = Enum.map(child_processes, fn({_, pid, _, [Child]}) -> pid end)
     {_, potato_pid, _, [Potato]} = potato_process
 
-    IO.inspect potato_pid
-    IO.inspect child_pids
+    [first_child | shuffled_children] = Enum.shuffle(child_pids)
+
+    Child.hot_potato(first_child, potato_pid, shuffled_children)
   end
 
   def random(n) do
